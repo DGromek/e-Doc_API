4150b01f8ba1805191084476d402cdfe6913bb53
DGromek <dawidgromek.1997@gmail.com>
2020-02-07 11:43:55 +0100

    Forgot to add Clinic Entity

---
+++ src/main/java/pl/edoc/entity/Clinic.java
@@ -0,0 +1,41 @@
+package pl.edoc.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name="clinic")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Clinic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    @Column(columnDefinition = "varchar(5)")
+    private String postal_code;
+
+    @NotEmpty
+    private String street;
+
+    @NotEmpty
+    @Column(columnDefinition = "varchar(4)")
+    private String houseNr;
+
+    private short flatNr;
+
+    @NotEmpty
+    @Column(columnDefinition = "varchar(11)")
+    private String phoneNumber;
+
+}
