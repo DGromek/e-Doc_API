059d984a77d44d8c08ec067b0ccd6489b3b0c1df
DGromek <dawidgromek.1997@gmail.com>
2020-02-07 08:53:45 +0100

    Initial project config

---
+++ src/main/java/pl/edoc/config/EDocApplication.java
@@ -0,0 +1,13 @@
+package pl.edoc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDocApplication.class, args);
+    }
+
+}
