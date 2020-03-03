8fe85228dbb4ccac4da8d60317289fb31d9fd324
DGromek <dawidgromek.1997@gmail.com>
2020-03-03 15:15:18 +0100

    Refactored name of project

--- src/main/java/pl/edoc/EDocAPIApplication.java
+++ src/main/java/pl/edoc/EDocAPIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EDocApplication {
+public class EDocAPIApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EDocApplication.class, args);
+        SpringApplication.run(EDocAPIApplication.class, args);
     }
 
 }
