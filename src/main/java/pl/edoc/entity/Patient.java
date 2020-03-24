a8a4c3136683c7c0383843a580debe2da45d8bd8
DGromek <dawidgromek.1997@gmail.com>
2020-03-24 15:03:30 +0100

    Added data.sql, changed Patient entity.

--- src/main/java/pl/edoc/entity/Patient.java
+++ src/main/java/pl/edoc/entity/Patient.java
@@ -7,6 +7,7 @@ import org.hibernate.validator.constraints.pl.PESEL;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,36 +31,31 @@ public class Patient {
     private String lastName;
 
     @NotEmpty
-    private char gender;
+    @Min(8)
+    private String password;
 
     @NotEmpty
     private String city;
 
     @NotEmpty
-    @Column(columnDefinition = "varchar(5)")
-    private String postal_code;
+    @Column(columnDefinition = "varchar(6)")
+    private String postalCode;
 
     @NotEmpty
     private String street;
 
     @NotEmpty
-    @Column(columnDefinition = "varchar(4)")
+    @Column(columnDefinition = "varchar(8)")
     private String houseNr;
 
-    private short flatNr;
-
-    @NotEmpty
     @Email
     private String email;
 
-    @NotEmpty
     @Column(columnDefinition = "varchar(11)")
     private String phoneNumber;
 
-    private boolean isInsured;
 
     @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL)
     private List<Appointment> appointments = new ArrayList<>();
-
 
 }
