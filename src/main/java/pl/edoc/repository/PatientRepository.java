5377fd16f2341e479731e1f27926ca3b5178c85c
DGromek <dawidgromek.1997@gmail.com>
2020-04-16 16:01:14 +0200

    Changed repostory type for Patient

--- src/main/java/pl/edoc/repository/PatientRepository.java
+++ src/main/java/pl/edoc/repository/PatientRepository.java
@@ -1,11 +1,12 @@
 package pl.edoc.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import pl.edoc.entity.Patient;
 
 @Repository
 @Transactional
-public interface PatientRepository extends CrudRepository<Patient, Long> {
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByPesel(String pesel);
 }
