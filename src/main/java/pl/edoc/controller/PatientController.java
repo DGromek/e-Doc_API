7c46b357f5e785f1be5b807821b7f2e6a9181979
DGromek <dawidgromek.1997@gmail.com>
2020-04-16 15:59:42 +0200

    Fixed returned http status

--- src/main/java/pl/edoc/controller/PatientController.java
+++ src/main/java/pl/edoc/controller/PatientController.java
@@ -1,20 +1,26 @@
 package pl.edoc.controller;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import pl.edoc.dto.Credintials;
 import pl.edoc.dto.PatientDTO;
 import pl.edoc.entity.Patient;
 import pl.edoc.services.PatientService;
 
-import javax.validation.Valid;
+import java.util.Date;
+
+import static pl.edoc.security.SecurityConfig.SECRET_512;
 
 @Controller
 @RequestMapping("/patient")
 public class PatientController {
-
     private PatientService patientService;
 
     @Autowired
@@ -29,7 +35,6 @@ public class PatientController {
 
     @PostMapping
     public ResponseEntity<Patient> postPatient(@RequestBody PatientDTO patientDTO) {
-        return new ResponseEntity<>(patientService.save(patientDTO), HttpStatus.OK);
+        return new ResponseEntity<>(patientService.save(patientDTO), HttpStatus.CREATED);
     }
-
 }
