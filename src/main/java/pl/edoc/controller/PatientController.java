010b319172ef9d718a1985e14dd37c764300c36c
DGromek <dawidgromek.1997@gmail.com>
2020-05-16 14:55:09 +0200

    Formated PatientController

--- src/main/java/pl/edoc/controller/PatientController.java
+++ src/main/java/pl/edoc/controller/PatientController.java
@@ -1,22 +1,16 @@
 package pl.edoc.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import pl.edoc.dto.Credintials;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.edoc.dto.PatientDTO;
 import pl.edoc.entity.Patient;
 import pl.edoc.services.PatientService;
-
-import java.util.Date;
-
-import static pl.edoc.security.SecurityConfig.SECRET_512;
 
 @Controller
 @RequestMapping("/patient")
