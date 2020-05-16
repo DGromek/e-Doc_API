54eb80f530982e356ab97a9a122a38e78024071e
DGromek <dawidgromek.1997@gmail.com>
2020-05-16 13:49:50 +0200

    Extended data.sql with sample data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,22 @@ INSERT INTO patient(pesel, first_name, last_name, password, postal_code, city, s
 ('68072503271', 'Adrian', 'Grzelczyk', '$2y$12$QUGnEfrCGvHcbZGlpXUW.eaGygpqDsdgGGo3HunGqnHlM6dzSkXFu', '05-271', 'Warszawa', 'Al. Jana Pawła II', '76 m.34', 'aGrzelczyk@onet.pl', '893 271 312'),
 ('55021121379', 'Grażyna', 'Zawadzka', '$2y$12$QUGnEfrCGvHcbZGlpXUW.eaGygpqDsdgGGo3HunGqnHlM6dzSkXFu', '90-162', 'Łódź', 'ul. Kardynała Stefana Wyszyńskiego', '42A', 'kicia21@wp.pl', '571 921 901'),
 ('99052101841', 'Karol', 'Krzynowicz', '$2y$12$QUGnEfrCGvHcbZGlpXUW.eaGygpqDsdgGGo3HunGqnHlM6dzSkXFu', '12-654', 'Stara Wieś', 'Stara Wieś', '231', 'KKrzyno99@gmail.com', '231 716 800');
+
+INSERT INTO doctor(first_name, last_name, speciality) VALUES
+('Jan', 'Kowalczyk', 'Ginekolog'),
+('Dariusz', 'Strzykała', 'Internista'),
+('Marek', 'Gormiński', 'Pediatra'),
+('Mateusz', 'Stanisławowski', 'Pediatra'),
+('Adam', 'Nagorski', 'Kardiolog'),
+('Karolina', 'Macążek', 'Chirurg'),
+('Sylwester', 'Fritz', 'Okulista');
+
+INSERT INTO clinic(name, postal_code, city, street, house_nr, flat_nr, phone_number) VALUES
+('Wojewódzkie Wielospecjalistyczne Centrum Onkologii i Traumatologii im. M. Kopernika w Łodzi', '90-001', 'Łódź', 'Pabianicka', '61', NULL, '426895000'),
+('Centrum Medyczne im. dr. L. Rydygiera Sp. z o.o.', '90-001', 'Łódź', 'Doktora Seweryna Sterlinga', '13', NULL, '426323465');
+
+INSERT INTO appointment(date_of_appointment, clinic_id, doctor_id, patient_pesel, status) VALUES
+('2020-06-02 13:30', 1, 2, '99052101841', 'DONE'),
+('2020-12-01 12:15', 2, 4, '99052101841', 'PENDING'),
+('2020-08-12 08:00', 1, 3, '99052101841', 'ACCEPTED');
+
