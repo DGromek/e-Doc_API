d27b70b25e8f4cc2fb080c12933283d3d3bbb654
DGromek <dawidgromek.1997@gmail.com>
2020-06-25 16:37:33 +0200

    Adding missed updated AppointmentService

--- src/main/java/pl/edoc/services/AppointmentService.java
+++ src/main/java/pl/edoc/services/AppointmentService.java
@@ -1,16 +1,26 @@
 package pl.edoc.services;
 
+import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.edoc.dto.AppointmentDTO;
 import pl.edoc.entity.Appointment;
+import pl.edoc.entity.Clinic;
+import pl.edoc.entity.Doctor;
+import pl.edoc.entity.Patient;
 import pl.edoc.repository.AppointmentRepository;
 
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Service
 public class AppointmentService {
 
     private AppointmentRepository appointmentRepository;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     public AppointmentService(AppointmentRepository appointmentRepository) {
@@ -19,5 +29,15 @@ public class AppointmentService {
 
     public Iterable<Appointment> findAllByPatient_Pesel(String pesel) {
         return appointmentRepository.findAllByPatient_Pesel(pesel);
+    }
+
+    public Appointment save(AppointmentDTO appointmentDto, String userPesel) {
+        Session session = entityManager.unwrap(Session.class);
+        Doctor doctor = session.get(Doctor.class, appointmentDto.getDoctorId());
+        Clinic clinic = session.get(Clinic.class, appointmentDto.getClinicId());
+        Patient patient = session.get(Patient.class, userPesel);
+
+        Appointment appointmentToSave = new Appointment(appointmentDto, patient, doctor, clinic);
+        return appointmentRepository.save(appointmentToSave);
     }
 }
