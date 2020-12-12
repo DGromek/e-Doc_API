7236c55a1b5f77340eb86e3c5eabce713b892799
DGromek <dawidgromek.1997@gmail.com>
2020-12-12 13:08:36 +0100

    Added missing annotations to the DoctorRepository

--- src/main/java/pl/edoc/repository/DoctorRepository.java
+++ src/main/java/pl/edoc/repository/DoctorRepository.java
@@ -2,8 +2,12 @@ package pl.edoc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.edoc.entity.Doctor;
 
+@Repository
+@Transactional
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 
     @Query(value = "SELECT * FROM doctor "
